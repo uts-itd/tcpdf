284c3d19c1aa83bf82d692c47a87b14d213f151a
Timothy Chandler <tim@zinios.com>
2020-01-17 16:28:24 +1100

    Academ version of tcpdf. Fixed for PHP 7.4.

---
+++ fonts/academreview.php
@@ -0,0 +1,15 @@
+<?php
+$type='TrueTypeUnicode';
+$name='AcademReview';
+$desc=array('Ascent'=>840,'Descent'=>-270,'CapHeight'=>840,'Flags'=>32,'FontBBox'=>'[-1 -48 900 763]','ItalicAngle'=>0,'StemV'=>70,'MissingWidth'=>600);
+$up=-2;
+$ut=1;
+$dw=600;
+$cw=array(
+0=>0,13=>0,32=>270,65=>891,67=>802,68=>810,80=>798,81=>803,83=>813);
+$enc='';
+$diff='';
+$file='academreview.z';
+$ctg='academreview.ctg.z';
+$originalsize=3076;
+// --- EOF ---
